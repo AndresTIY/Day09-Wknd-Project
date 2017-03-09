867187ccfad95eca83d8cdec15a1c42d684cbffa
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-09 15:13:12 -0600

    finding values

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var input1 = document.querySelector('#one');
+var input2 = document.querySelector('#two');
+var input3 = document.querySelector('#three');
+var input4 = document.querySelector('#four');
+var input5 = document.querySelector('#five');
+var input6 = document.querySelector('#six');
+var input7 = document.querySelector('#seven');
+var input8 = document.querySelector('#eight');
+var input9 = document.querySelector('#nine');
+var input0 = document.querySelector('#zero');
+
+//input1.value = 1
+
+input7.addEventListener('click', function click(){
+  console.log('it works!');
+
+})
+
+
+//.all[9].childNodes[1].value = 7
+// button
+// .all[10]
