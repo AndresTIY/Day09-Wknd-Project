9c8872e320f08fdd8b366daa3c27b75cfd5be236
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-10 16:16:56 -0600

    all basic op working

--- main.js
+++ main.js
@@ -20,27 +20,38 @@ clear.addEventListener('click', function click(){
   operatorSel = '';
 })
 
+
 run.addEventListener('click', function operation(){
   if (operatorSel === "+"){
     display.textContent = Number(memory) + Number(memory2);
+    memory = '';
+    memory2 = '';
+    selectedNum = '';
+    selectedNum2 = '';
+    operatorSel = '';
   } else if (operatorSel === "-"){
     display.textContent = Number(memory) - Number(memory2);
+    memory = '';
+    memory2 = '';
+    selectedNum = '';
+    selectedNum2 = '';
+    operatorSel = '';
   } else if (operatorSel === "/"){
     display.textContent = Number(memory) / Number(memory2);
+    memory = '';
+    memory2 = '';
+    selectedNum = '';
+    selectedNum2 = '';
+    operatorSel = '';
   } else if (operatorSel === "X"){
     display.textContent = Number(memory) * Number(memory2);
+    memory = '';
+    memory2 = '';
+    selectedNum = '';
+    selectedNum2 = '';
+    operatorSel = '';
   }
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -81,73 +92,122 @@ operator[3].addEventListener('click', function click (){
 
 
 inputAll[0].addEventListener('click', function click(){
-
   if (operatorSel.length === 1) {
     selectedNum2 = inputAll[0].value;
     memory2 = memory2 + selectedNum2;
-    display.textContent = '';
     display.textContent = memory2
   }
-
   else if (operatorSel === '') {
-
-
   selectedNum = inputAll[0].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-
-}
-})
-
-
-
-
-
-
-
-
+  }
+});
 inputAll[1].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[1].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[1].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[2].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[2].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[2].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[3].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[3].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[3].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[4].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[4].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[4].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[5].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[5].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[5].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[6].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[6].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[6].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[7].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[7].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[7].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[8].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[8].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[8].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
 inputAll[9].addEventListener('click', function click(){
+  if (operatorSel.length === 1) {
+    selectedNum2 = inputAll[9].value;
+    memory2 = memory2 + selectedNum2;
+    display.textContent = memory2
+  }
+  else if (operatorSel === '') {
   selectedNum = inputAll[9].value;
   memory = memory + selectedNum;
   display.textContent = memory;
-})
+  }
+});
