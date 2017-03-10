979b94feac66c448cf496d9996cbec53016b01b6
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-10 12:47:00 -0600

    working through

--- main.js
+++ main.js
@@ -9,15 +9,123 @@ var input7 = document.querySelector('#seven');
 var input8 = document.querySelector('#eight');
 var input9 = document.querySelector('#nine');
 var input0 = document.querySelector('#zero');
+var inputAll = document.querySelectorAll('button');
+var inputAlt = document.getElementsByClassName('num');
+
+var operator = document.getElementsByClassName('operator');
 
 //input1.value = 1
 
-input7.addEventListener('click', function click(){
-  console.log('it works!');
+// input7.addEventListener('click', function click(){
+//   var first = input7.value;
+// })
 
-})
+//inputAll[0].value = 7
+// inputAll[0].addEventListener('click', function click(){
+//   console.log(inputAll[0].value);
+// })
+// that works
+//gives us the value
+
+//what if...
+  // function recurse(){
+  // for (var i = 0; i < 15; i++){
+  //   inputAll[i].addEventListener('click', function click(){
+  //     console.log(inputAll[i].value);
+  //     })
+  //   }
+  // };
+  //
+  // recurse();
+  //didn't work, inputAll[i].value comes out as undefined
+var first;
+var second;
 
 
-//.all[9].childNodes[1].value = 7
-// button
-// .all[10]
+
+// inputAlt[0].addEventListener('click', function click(){
+//   first = inputAlt[0].value;
+//   arr[0] = first
+//
+// });
+inputAlt[0].addEventListener('click', function click(){
+  first = inputAlt[0].value;
+});
+inputAlt[1].addEventListener('click', function click(){
+  first = inputAlt[1].value;
+});
+inputAlt[2].addEventListener('click', function click(){
+  first = inputAlt[2].value;
+});
+inputAlt[3].addEventListener('click', function click(){
+  first = inputAlt[3].value;
+});
+inputAlt[4].addEventListener('click', function click(){
+  first = inputAlt[4].value;
+});
+inputAlt[5].addEventListener('click', function click(){
+  first = inputAlt[5].value;
+});
+inputAlt[6].addEventListener('click', function click(){
+  first = inputAlt[6].value;
+});
+inputAlt[7].addEventListener('click', function click(){
+  first = inputAlt[7].value;
+});
+inputAlt[8].addEventListener('click', function click(){
+  first = inputAlt[8].value;
+});
+inputAlt[9].addEventListener('click', function click(){
+  first = inputAlt[9].value;
+});
+
+
+
+
+
+
+
+
+if (first === first) {
+
+inputAlt[0].addEventListener('click', function click(){
+  second = inputAlt[0].value;
+});
+inputAlt[1].addEventListener('click', function click(){
+  second = inputAlt[1].value;
+});
+inputAlt[2].addEventListener('click', function click(){
+  second = inputAlt[2].value;
+});
+inputAlt[3].addEventListener('click', function click(){
+  second = inputAlt[3].value;
+});
+inputAlt[4].addEventListener('click', function click(){
+  second = inputAlt[4].value;
+});
+inputAlt[5].addEventListener('click', function click(){
+  second = inputAlt[5].value;
+});
+inputAlt[6].addEventListener('click', function click(){
+  second = inputAlt[6].value;
+});
+inputAlt[7].addEventListener('click', function click(){
+  second = inputAlt[7].value;
+});
+inputAlt[8].addEventListener('click', function click(){
+  second = inputAlt[8].value;
+});
+inputAlt[9].addEventListener('click', function click(){
+  second = inputAlt[9].value;
+});
+
+};
+// when i run these, first = second :(
+
+
+
+
+// }
+// operator[0].addEventListener('click', function add(first, second){
+//   console.log(first + second)
+// })
